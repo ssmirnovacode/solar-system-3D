70bb77addd3ba298f13b640afc9c72e6def2a29e
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-07-04 13:15:17 +0200

    more planets added

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,8 +3,12 @@
 const SUN_SIZE = 2; // 109
 const EARTH_SIZE = 0.2; // 1
 const MERCURY_SIZE = 0.08; // .38
-//const MARS_SIZE = 0.13; // 0.53
+const MARS_SIZE = 0.13; // 0.53
 const VENUS_SIZE = 0.18; // 0.95
+const JUPITER_SIZE = 1.2; // 11.21
+const SATURN_SIZE = 0.9; // 9.45
+const URANUS_SIZE = 0.8; // 4.01
+const NEPTUNE_SIZE = 0.7; // 3.88
 
 const POSITIONS = {
   earth: {
@@ -32,6 +36,48 @@ const POSITIONS = {
     y: 0,
     z: 0,
   },
+  jupiter: {
+    x:
+      SUN_SIZE + (MERCURY_SIZE + VENUS_SIZE + EARTH_SIZE + MARS_SIZE) * 2 + 5.2,
+    y: 0,
+    z: 0,
+  },
+  saturn: {
+    x:
+      SUN_SIZE +
+      (MERCURY_SIZE + VENUS_SIZE + EARTH_SIZE + MARS_SIZE + JUPITER_SIZE) * 2 +
+      9.58,
+    y: 0,
+    z: 0,
+  },
+  uranus: {
+    x:
+      SUN_SIZE +
+      (MERCURY_SIZE +
+        VENUS_SIZE +
+        EARTH_SIZE +
+        MARS_SIZE +
+        JUPITER_SIZE +
+        SATURN_SIZE) *
+        2 +
+      19.58,
+    y: 0,
+    z: 0,
+  },
+  neptune: {
+    x:
+      SUN_SIZE +
+      (MERCURY_SIZE +
+        VENUS_SIZE +
+        EARTH_SIZE +
+        MARS_SIZE +
+        JUPITER_SIZE +
+        URANUS_SIZE) *
+        2 +
+      30.07,
+    y: 0,
+    z: 0,
+  },
   //   sun: {
   //     x: 0,
   //     y: 0,
@@ -41,11 +87,15 @@ const POSITIONS = {
 
 const SIZES = {
   // TODO add formula depending on sun size
-  earth: 0.2, // 1
+  earth: EARTH_SIZE, // 1
   sun: SUN_SIZE, // 109,
-  mercury: 0.08, // .38
-  venus: 0.18, // 0.95
-  mars: 0.13, // 0.53
+  mercury: MERCURY_SIZE, // .38
+  venus: VENUS_SIZE, // 0.95
+  mars: MARS_SIZE, // 0.53
+  jupiter: JUPITER_SIZE,
+  saturn: SATURN_SIZE,
+  uranus: URANUS_SIZE,
+  neptune: NEPTUNE_SIZE,
 };
 
 const PLANETS = {
@@ -73,6 +123,26 @@ const PLANETS = {
     size: SIZES.venus,
     coords: POSITIONS.venus,
     color: 0xff99ff,
+  },
+  jupiter: {
+    size: SIZES.jupiter,
+    coords: POSITIONS.jupiter,
+    color: 0x993333,
+  },
+  saturn: {
+    size: SIZES.saturn,
+    coords: POSITIONS.saturn,
+    color: 0x996633,
+  },
+  uranus: {
+    size: SIZES.uranus,
+    coords: POSITIONS.uranus,
+    color: 0x003399,
+  },
+  neptune: {
+    size: SIZES.neptune,
+    coords: POSITIONS.neptune,
+    color: 0x339966,
   },
 };
 
