8c50495af67d645d99b60465d105504f839e5494
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-07-04 15:50:30 +0200

    measurements refactor

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,101 +1,75 @@
-// positions
+const multiplier = 10; // 1 AU (for viewing purposes)
 
-const SUN_SIZE = 2; // 109 (lets imagine sun is only 10x bigger than Earth)
-const EARTH_SIZE = 0.2; // 1
-const MERCURY_SIZE = 0.08; // .38
-const MARS_SIZE = 0.13; // 0.53
-const VENUS_SIZE = 0.18; // 0.95
-const JUPITER_SIZE = 1.2; // 11.21
-const SATURN_SIZE = 0.9; // 9.45
-const URANUS_SIZE = 0.8; // 4.01
-const NEPTUNE_SIZE = 0.7; // 3.88
+const EARTH_RADIUS = 0.15; // 1
+const SUN_RADIUS = EARTH_RADIUS * 10; // 109 // For viewing purposes, we will imagine that the sun is 10 times smaller than in reality
+const MERCURY_RADIUS = EARTH_RADIUS * 0.38; // .38
+const MARS_RADIUS = EARTH_RADIUS * 0.53; // 0.53
+const VENUS_RADIUS = EARTH_RADIUS * 0.95; // 0.95
+const JUPITER_RADIUS = EARTH_RADIUS * 11.21; // 11.21
+const SATURN_RADIUS = EARTH_RADIUS * 9.45; // 9.45
+const URANUS_RADIUS = EARTH_RADIUS * 4.01; // 4.01
+const NEPTUNE_RADIUS = EARTH_RADIUS * 3.88; // 3.88
 
 const POSITIONS = {
-  earth: {
-    x: SUN_SIZE + (MERCURY_SIZE + VENUS_SIZE) * 2 + 1,
-    y: 0,
-    z: 0,
-  },
   sun: {
     x: 0,
     y: 0,
     z: 0,
   },
+
   mercury: {
-    x: SUN_SIZE + 0.39, //0.39
+    x: 0.39 * multiplier,
     y: 0,
     z: 0,
   },
   venus: {
-    x: SUN_SIZE + MERCURY_SIZE * 2 + 0.72,
+    x: 0.72 * multiplier,
+    y: 0,
+    z: 0,
+  },
+  earth: {
+    x: 1 * multiplier,
     y: 0,
     z: 0,
   },
   mars: {
-    x: SUN_SIZE + (MERCURY_SIZE + VENUS_SIZE + EARTH_SIZE) * 2 + 1.52,
+    x: 1.52 * multiplier,
     y: 0,
     z: 0,
   },
   jupiter: {
-    x:
-      SUN_SIZE + (MERCURY_SIZE + VENUS_SIZE + EARTH_SIZE + MARS_SIZE) * 2 + 5.2,
+    x: 5.2 * multiplier,
     y: 0,
     z: 0,
   },
   saturn: {
-    x:
-      SUN_SIZE +
-      (MERCURY_SIZE + VENUS_SIZE + EARTH_SIZE + MARS_SIZE + JUPITER_SIZE) * 2 +
-      9.58,
+    x: 9.58 * multiplier,
     y: 0,
     z: 0,
   },
   uranus: {
-    x:
-      SUN_SIZE +
-      (MERCURY_SIZE +
-        VENUS_SIZE +
-        EARTH_SIZE +
-        MARS_SIZE +
-        JUPITER_SIZE +
-        SATURN_SIZE) *
-        2 +
-      19.58,
+    x: 19.58 * multiplier,
     y: 0,
     z: 0,
   },
   neptune: {
-    x:
-      SUN_SIZE +
-      (MERCURY_SIZE +
-        VENUS_SIZE +
-        EARTH_SIZE +
-        MARS_SIZE +
-        JUPITER_SIZE +
-        URANUS_SIZE) *
-        2 +
-      30.07,
+    x: 30.07 * multiplier,
     y: 0,
     z: 0,
   },
-  //   sun: {
-  //     x: 0,
-  //     y: 0,
-  //     z: 0,
-  //   },
 };
 
 const SIZES = {
   // TODO add formula depending on sun size
-  earth: EARTH_SIZE, // 1
-  sun: SUN_SIZE, // 109,
-  mercury: MERCURY_SIZE, // .38
-  venus: VENUS_SIZE, // 0.95
-  mars: MARS_SIZE, // 0.53
-  jupiter: JUPITER_SIZE,
-  saturn: SATURN_SIZE,
-  uranus: URANUS_SIZE,
-  neptune: NEPTUNE_SIZE,
+  earth: EARTH_RADIUS, // 1
+  sun: SUN_RADIUS, // 109,
+  mercury: MERCURY_RADIUS, // .38
+  venus: VENUS_RADIUS, // 0.95
+  mars: MARS_RADIUS, // 0.53
+  jupiter: JUPITER_RADIUS,
+  saturn: SATURN_RADIUS,
+  uranus: URANUS_RADIUS,
+  neptune: NEPTUNE_RADIUS,
 };
 
 const PLANETS = {
